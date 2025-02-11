05a3d97c534398bb962ad7f89ff6bda48fe06d4d
Jeremy Inoa Fortuna <jeremyinoa67@gmail.com>
2025-02-11 19:55:57 -0400

    [ FEAT ] : main files completed

---
+++ commands.sh
@@ -0,0 +1,31 @@
+# STEPS TO GET THE PROJECT RUNNING:
+
+# 1. cd to the project directory
+# 2. ./commands.sh
+
+# 3. Run this command to create Environment Folder
+    python3 -m venv env
+    # If windows
+    python -m venv env
+
+# 4. Run this command to activate Environment Folder
+    source venv/bin/activate
+    # If windows
+    .\venv\Scripts\activate
+
+# 5. Run this command to install requirements
+    pip install -r requirements.txt
+
+# 6. Run this command to build the docker image and start the container:
+    docker compose up --build
+    # or, to run it in background
+    docker compose up --d
+
+#  ALTERNATIVE COMMANDS:
+    # - Run this command to run the project
+    fastapi run main.py
+    # For Development
+    fastapi dev main.py
+
+    # - Run this command to stop the project
+        # Hotkeys: ctrl + c
